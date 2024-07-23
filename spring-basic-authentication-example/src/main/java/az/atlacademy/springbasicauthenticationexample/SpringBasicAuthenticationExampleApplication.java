3088bc1ab15795feefdeb6fd03206448cd969f28
Ilgar <126610106+ilgarmustafayev22@users.noreply.github.com>
2024-07-23 18:27:13 +0400

    module03 spring-basic-authentication class code attached

---
+++ spring-basic-authentication-example/src/main/java/az/atlacademy/springbasicauthenticationexample/SpringBasicAuthenticationExampleApplication.java
@@ -0,0 +1,13 @@
+package az.atlacademy.springbasicauthenticationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAuthenticationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicAuthenticationExampleApplication.class, args);
+    }
+
+}
---
+++ spring-basic-authentication-example/src/test/java/az/atlacademy/springbasicauthenticationexample/SpringBasicAuthenticationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atlacademy.springbasicauthenticationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicAuthenticationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
